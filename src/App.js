236c3116fb4be4b04546a89512d3b7530823fb91
MekImEne <fi_mekideche@esi.dz>
2022-10-21 21:24:46 +0100

    initiate project + template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+import { EXERCICES } from './utils/items';
+import { Divider } from '@mui/material';
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+   <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          variant="scrollable"
+          scrollButtons="auto"
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {
+            EXERCICES.map((exo,index)=> <Tab style={{fontWeight:"bold"}} label={exo.title} {...a11yProps(index)} />)
+          }
+        </Tabs>
+      </Box>
+      {
+        EXERCICES.map((exo, index) =>
+          <TabPanel key={exo.key} value={value} index={index}>
+            <Typography style={{fontWeight:"bolder"}} variant='h6' >{exo.title}</Typography> 
+            <Divider />
+            {exo.component}
+          </TabPanel>
+        )
+      }
+    </Box>
   );
 }
 
---
+++ src/utils/items.js
@@ -0,0 +1,82 @@
+export const EXERCICES = [
+    {
+        key: 1,
+        title: "JS Basics",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 2,
+        title: "JS Fundamental I",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 3,
+        title: "JS Fundamental II",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 4,
+        title: "JS Functions",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 5,
+        title: "JS Recursion",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 6,
+        title: "JS Conditional Statements and loops",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 7,
+        title: "JS Array",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 8,
+        title: "JS Date",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 9,
+        title: "JS String",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 10,
+        title: "JS Math",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 11,
+        title: "JS Validation with Regular expression",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 12,
+        title: "JS HTML DOM",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 13,
+        title: "JS Drawing",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 14,
+        title: "JS Object",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 15,
+        title: "JS Basic Validation without Regular expression",
+        component: <h1>Component here</h1>
+    },
+    {
+        key: 16,
+        title: "JS Searching and Sorting Algorithm",
+        component: <h1>Component here</h1>
+    },
+]
